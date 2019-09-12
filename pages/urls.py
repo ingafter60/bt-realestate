49e799e60d65b2787db8ae8888a157d4748946f6
ing <inyoman_gurnitha@yahoo.com>
2019-09-12 15:42:14 +0700

    2. Pages Templates & Base Layout

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path('', views.index, name='home' )
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
 ]
